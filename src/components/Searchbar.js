dd0b1062bd9a59b2ac9c5c5a3500863e2727b489
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-05-11 21:34:00 -0700

    Successfully hitting deezer, getting results, click handler to set selected song

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -4,26 +4,35 @@ import { axiosWithAuth } from "../utils/axiosWithAuth"
 //I dont believe deezer requires an API key
 //const { API_KEY } = process.env
 
-const API_URL = "https://api.deezer.com/search?q="
+const API_URL =
+  "https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=";
 
 const Searchbar = () => {
-    const [query, setQuery] = useState("")
+    const [query, setQuery] = useState("");
 
-    const [results, setResults] = useState([])
+    const [results, setResults] = useState([]);
 
-    const fullQuery = `${API_URL}${query}`
+    const [selectedSong, setSelectedSong] = useState({
+        title_short: "",
+        preview: "",
+        artist: "",
+        deezer_id: ""
+    });
 
-    const getInfo = () =>
+    const fullQuery = `${API_URL}${query}`;
+
+    const getInfo = () => {
         console.log(fullQuery);
-    axiosWithAuth()
-        .get(`${fullQuery}&limit=7`)
-        .then(res => {
-            console.log('res', res)
-            setResults(res.data.data)
-        })
-        .catch(err => {
-            console.log("unable to suggest artist and/or song", err)
-        })
+        axiosWithAuth()
+          .get(`${fullQuery}&limit=7`)
+          .then((res) => {
+            console.log("res", res);
+            setResults(res.data.data);
+          })
+          .catch((err) => {
+            console.log("unable to suggest artist and/or song", err);
+          });
+    }
 
     useEffect(() => {
         if (query.length >= 2) getInfo()
@@ -34,26 +43,34 @@ const Searchbar = () => {
         setQuery(event.target.value)
     }
 
-    return (
-        <>
-            <input placeholder="Search Artist and/or Song Title" onChange={handleInputChange} />
+    const handleClickSong = event => {
+        const songItem = event.target.getAttribute("data-index");
+        setSelectedSong({
+            title_short: results[songItem].title_short,
+            preview: results[songItem].preview,
+            artist: results[songItem].artist.name,
+            deezer_id: results[songItem].id
+        });
+    };
 
-            <ul>
-                {results && (results > 0) ? (
-                    results.map(res => (
-                        <li key={res.name}>{res.name}</li>
-                    ))
-                ) : (
-                        console.log("broken")
-                    )}
-                {/* {if (results) {
-                    results.map(res => (
-                        <li key={res.name}>{res.name}</li>
-                    ))}
-                } */}
-            </ul>
-        </>
-    )
+    return (
+      <>
+        <input
+          placeholder="Search Artist and/or Song Title"
+          onChange={handleInputChange}
+        />
+
+        <ul>
+          {results && results.length > 0
+            ? results.map((res, index) => (
+                <li onClick={handleClickSong} key={index} data-index={index}>
+                  {res.artist.name} - {res.title}
+                </li>
+              ))
+            : console.log("broken")}
+        </ul>
+      </>
+    );
 }
 
 export default Searchbar

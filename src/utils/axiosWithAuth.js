4e1af62d7e864b979c38d758f29a683ad0a749ab
jtwray <you@example.com>
2020-05-12 22:25:57 -0400

    add video-react mp4 player-- has lots of aria- goodies built in, a fulscreen button, a test mp4. -- styled a medium sized video top center to match the wireframe above suggetsted videos.

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
+    baseURL: `process.env.BASEURL`,
     headers: {
+      Authorization: token,
       "Content-Type": "application/json",
-      Authorization: `${token}`,
     },
   });
 };

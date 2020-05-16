6328d9912a0d7070ed92a5faf14f496c4407c87f
jtwray <you@example.com>
2020-05-16 17:11:16 -0400

    routes the stores videoList array down to SingleVideoPage and sorts out the video matching url's videoID.. adjust props on future commit to update  src url to match selected video

--- src/components/SingleVideoPage.js
+++ src/components/SingleVideoPage.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { useParams, useRouteMatch } from "react-router-dom";
-import {useSelector} from"react-redux"
+import { connect, useSelector } from "react-redux";
+import { useParams, useRouteMatch, withRouter, Link } from "react-router-dom";
+import { getVideos } from "../store/actions";
 import Video from "./Video";
 
-const SingleVideoPage = (props) => {
-
-  const videos = useSelector(state => state.videoList)
+const SingleVideoPage = ({ stateVL }) => {
   const { videoID } = useParams();
-  const correctVideo = videos.find(
-    (video) => video.id === Number(videoID)
-  );
-console.log(correctVideo)
-  console.log(props);
+console.log("line9SVP",{videoID})
+  const correctVideo = stateVL.find((video) => video.id === Number(videoID));
+
+  console.log("#########################--->>>>>", { correctVideo });
 
   return (
     <>
+      {console.log("#########################--->>>>>", { correctVideo })}
       <Video video={correctVideo} />
     </>
   );
 };
 
-export default SingleVideoPage;
+const mapStateToProps = (state) => ({
+  getVideosStart: state.getVideosStart,
+  postVideoSuccess: state.postVideoSuccess,
+  loginSuccess: state.loginSuccess,
+  stateVL: state.videoList,
+});
+
+export default connect(mapStateToProps, {
+  getVideos,
+})(withRouter(SingleVideoPage));

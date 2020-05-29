a06bb256aa7ef29846f5d1467d411078bbde102d
Jimmique <jimmique88@gmail.com>
2020-05-29 00:28:10 -0700

    was able to get initial console log onto solo page, however, the solution to the loading state for the single video page has already been resolved on another commit by Courtney and Jimmy.

--- src/components/SingleVideoPage.js
+++ src/components/SingleVideoPage.js
@@ -1,39 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { connect, useSelector } from "react-redux";
 import { useParams, useRouteMatch, withRouter, Link } from "react-router-dom";
-import { getSingleVideo } from "../store/actions";
+import { getVideos } from "../store/actions";
 import Video from "./Video";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const SingleVideoPage = ({ videos }) => {
+const SingleVideoPage = props => {
   const { videoID } = useParams();
+  console.log(videoID);
+
+  const videos = [
+    {
+      video_title: "",
+      location: "youtube.com/video",
+      song_id: "",
+      title: "",
+      artist_name: ""
+    }
+  ];
 
   const correctVideo = videos.find(video => video.id === Number(videoID));
 
-  // useEffect(() => {
-  //   if (video.id !== Number(videoID)) {
-  //     return <Loading />;
-  //   }
-  // });
+  useEffect(() => {
+    //Solo video props will need to be passed through here
+    console.log("I am inside of the Single Video Page");
+  });
 
   return (
     <>
       <Video video={correctVideo} />
-      {/* <h1>Title: {videos.title}</h1>
-      <h3>Creator: {videos.username}</h3> */}
     </>
   );
 };
 
-const mapStateToProps = state => ({
-  getSingleVideoStart: state.getSingleVideoStart,
-  postSingleVideoSuccess: state.postSingleVideoSuccess,
-  loginSuccess: state.loginSuccess,
-  videos: state.videoList
-});
+const mapStateToProps = state => {
+  return {
+    getVideosStart: state.getVideosStart,
+    postVideoSuccess: state.postVideoSuccess,
+    loginSuccess: state.loginSuccess,
+    videos: state.videoList
+  };
+};
 
 export default connect(
   mapStateToProps,
   {
-    getSingleVideo
+    getVideos
   }
 )(withRouter(SingleVideoPage));
+
+//Courtney's loading state from Loading.js
+
+/*const { location } = state;
+
+useEffect(() => {
+  axiosWithAuth
+    .get("/video/:id")
+    .then((res) => {
+      console.log(res);
+      if (location.status === null) {
+        const pingTimerId = setTimeout(() => {
+          setIsLoading(false);
+        }, 5000);
+      } else if ((location.status = "url")) {
+        return setIsLoading(videoData);
+      }
+    })
+    .catch((err) => {
+      console.log("Server error while getting video", err);
+    });
+
+  return () => {
+    clearTimeout(pingTimerId);
+  };
+});*/

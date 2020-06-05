b9826f4c471d25ddf7c1dce663158ef2de36f947
Jimmique <jimmique88@gmail.com>
2020-06-04 23:06:30 -0700

    Removed commented out axios call and not for username greeting

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -11,22 +11,6 @@ const UserPage = props => {
 
   useEffect(() => {
     props.getUserVideos(localStorage.getItem("token"), username);
-    /*axios
-      .get(`http://localhost:5050/api/users/username/${username}`)
-      .then(res => {
-        //console.log(res.data[0].id);
-        axios
-          .get(`http://localhost:5050/api/videos/user/${res.data[0].id}`)
-          .then(res => {
-            setVideos(res.data);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      })
-      .catch(err => {
-        console.log(err);
-      });*/
   }, [username]);
 
   let greeting;
@@ -38,12 +22,10 @@ const UserPage = props => {
     greeting = `${username} Videos`;
     console.log(username);
   }
-
   return (
     <>
       <Container>
         <div>
-          {/* Need to display user's username in Welcome*/}
           <h1>{greeting}</h1>
           <p>
             Now that you're logged in, lets{" "}

b013997d3d96d2ecd292761a24137b77eac253b0
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-06-09 23:23:40 -0700

    Updated single video page logic to account for multiple different video-status states

--- src/components/SingleVideoPage.js
+++ src/components/SingleVideoPage.js
@@ -17,31 +17,60 @@ const SingleVideoPage = (props) => {
 
   return (
     <>
-      {props.singleVideo === "initializing" ? (
+      {/* {credits < 30 ? "freshman" 
+      : credits <= 59 ? "sophomore" 
+      : credits <= 89 ? "junior" 
+      : "senior"} */}
+      {props.singleVideo.video_status === "creating" ? (
         <>
-          <h2>Loading...</h2>
+          <h2>
+            We're crunching the numbers on this one, STILL, check back in a few
+            minutes...
+          </h2>
+        </>
+      ) : props.singleVideo.video_status === "successful" ? (
+        <>
+          <Player
+            poster={props.singleVideo.thumbnail}
+            playsInline
+            src={props.singleVideo.location}
+          />
+          <h2>{props.singleVideo.video_title}</h2>
+          <h3>{props.singleVideo.title}</h3>
+          <h3>{props.singleVideo.artist_name}</h3>
+        </>
+      ) : props.singleVideo.video_status === "failed" ? (
+        <>
+          <h2>Something failed, we're so, so, so deeply sorry</h2>
         </>
       ) : (
-        <>
-          {props.singleVideo.location &&
-          props.singleVideo.location.length > 1 ? (
-            <>
-              <Player
-                poster={props.singleVideo.thumbnail}
-                playsInline
-                src={props.singleVideo.location}
-              />
-              <h2>{props.singleVideo.video_title}</h2>
-              <h3>{props.singleVideo.title}</h3>
-              <h3>{props.singleVideo.artist_name}</h3>
-            </>
-          ) : (
-            <>
-              <h2>We're still chugging away on this one</h2>
-            </>
-          )}
-        </>
+        console.log("end of ternary????")
       )}
+      {/* // {props.singleVideo === "initializing" ? (
+      //   <>
+      //     <h2>Loading...</h2>
+      //   </>
+      // ) : (
+      //   <>
+      //     {props.singleVideo.location &&
+      //     props.singleVideo.location.length > 1 ? (
+      //       <>
+      //         <Player
+      //           poster={props.singleVideo.thumbnail}
+      //           playsInline
+      //           src={props.singleVideo.location}
+      //         />
+      //         <h2>{props.singleVideo.video_title}</h2>
+      //         <h3>{props.singleVideo.title}</h3>
+      //         <h3>{props.singleVideo.artist_name}</h3>
+      //       </>
+      //     ) : (
+      //       <>
+      //         <h2>We're still chugging away on this one</h2>
+      //       </>
+      //     )}
+      //   </>
+      // )} */}
     </>
   );
 };

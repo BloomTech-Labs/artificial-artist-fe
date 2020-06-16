00c7bc27c8bf63bce2dfe8d39481f922bffbb74d
CourtneyJa <courtney.jackson08234@gmail.com>
2020-06-16 00:31:50 -0400

    video search working, pushes to video in video player

--- src/components/SearchVideos.js
+++ src/components/SearchVideos.js
@@ -1,27 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { connect } from "react-redux";
-import { withRouter, useHistory } from "react-router-dom";
+import { withRouter, useHistory, Link } from "react-router-dom";
 import { getVideos } from "../store/actions";
 import Fuse from "fuse.js";
 import VideoList from "./VideoList";
+import Video from "./Video";
 
 function Search(props) {
   const history = useHistory();
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
-
-  // useEffect(() => {
-  //   async function preload() {
-  //     try {
-  //       await getVideos();
-  //       console.log(props.videos);
-  //     } catch (err) {
-  //       console.log(err, "i dont work");
-  //     }
-  //   }
-  //   preload();
-  // }, []);
 
   useEffect(() => {
     props.getVideos();
@@ -31,52 +19,41 @@ function Search(props) {
     if (props.videos && props.videos.length > 0) {
       setResults(fuse.search(query));
     }
-    console.log(query, "I work second");
   }, [query]);
 
-  const handleSelect = (e) => {
-    const searchedItemIndex = e.target.getAttribute("data-index");
-    const searchedItem = results[searchedItemIndex];
-    history.push("/videos", { searchedItem });
+  const renderItems = () => {
+    return (
+      <>
+        {query.length > 2 ? (
+          <>
+            {results.length > 0 ? (
+              results.map((result) => {
+                return (
+                  <Link to={`/videos/${result.item.id}`} key={result.item.id}>
+                    {result.item.title}
+                  </Link>
+                );
+              })
+            ) : (
+              <div>No matching videos found</div>
+            )}
+          </>
+        ) : (
+          <VideoList />
+        )}
+      </>
+    );
   };
-
-  // const renderItems = () => {
-  //   return results.length > 0 ? (
-  //     results.map((item, i) => {
-  //       return (
-  //         <li onClick={handleSelect} key={i} data-index={i}>
-  //           {item.id}---{item.title}
-  //         </li>
-  //       );
-  //     })
-  //   ) : (
-  //     <div>
-  //       <h4>No matching video found</h4>
-  //       <VideoList />
-  //     </div>
-  //   );
-  // };
 
   const fuseOptions = {
     shouldSort: true,
     threshold: 0.4,
     includeScore: true,
-    // location: 0,
-    // distance: 50,
-    // maxPatternLength: 12,
     minMatchCharLength: 2,
     keys: ["title", "video_title", "artist_name"],
   };
 
   const fuse = new Fuse(props.videos, fuseOptions);
-
-  //const results = fuse.search(query);
-
-  // const videoResults = query ? (
-  //   results.map((video) => video.item)
-  // ) : (
-  //   <VideoList />
-  // );
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -98,10 +75,8 @@ function Search(props) {
           />
         </form>
         <div>
-          {results && results.map((result) => <h1>{result.item.title}</h1>)}
+          <ul>{renderItems()}</ul>
         </div>
-
-        <div>{/* <ul>{renderItems()}</ul> */}</div>
       </div>
     </>
   );

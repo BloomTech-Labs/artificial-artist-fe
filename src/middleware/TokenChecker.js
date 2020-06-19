266d4f6df09e5bd9d949692de7b51a4ebec7948d
Jimmique <jimmique88@gmail.com>
2020-06-18 18:05:26 -0700

    checked tokens expiration against Date.Now to confirm it is still valid.

--- src/middleware/TokenChecker.js
+++ src/middleware/TokenChecker.js
@@ -6,6 +6,11 @@ export const TokenChecker = () => {
 
   if (token) {
     console.log("here's your token");
+    //Verify the token's expiration against Date.now
+    if (token.exp * 1000 > Date.now()) {
+      //The login is still valid
+      console.log("valid login");
+    }
     return token;
   }
 };

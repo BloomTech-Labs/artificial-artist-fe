10584ada67ffe099164609bc2ff34b97401c55df
Jimmique <jimmique88@gmail.com>
2020-06-23 20:57:40 -0700

    Changed "style" to "styled" for styled-components

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -4,15 +4,14 @@ import { Link, useParams, withRouter } from "react-router-dom";
 import { getUserVideos } from "../store/actions";
 import Video from "./Video";
 import SecondaryNav from "./SecondaryNav";
-import style from "styled-components";
 import styled from "styled-components";
 import { linkSync } from "fs";
 
-const VideoListContainer = style.div`
-  background-color: #0E0429;
+const VideoListContainer = styled.div`
+  background-color: #0e0429;
 `;
 
-const Container = style.div`
+const Container = styled.div`
   margin: 0 auto;
   width: 60%;
   padding: 20px 0 60px;
@@ -21,40 +20,40 @@ const Container = style.div`
   justify-content: space-between;
 `;
 
-const VideoContainer = style.div`
+const VideoContainer = styled.div`
   width: 45%;
 `;
 
-const VideoTitle = style.h3`
-  color: #FCFC0B;
+const VideoTitle = styled.h3`
+  color: #fcfc0b;
   font-size: 24px;
   padding: 20px 0;
 `;
 
-const SmallerVideo = style.div`
-  background-color: #44E0F6;
-  border: 10px solid #44E0F6;
-  box-shadow: 10px 10px 0px 0px rgba(125,250,154,1), 10px 10px 23px 2px rgba(0,0,0,0.46);
+const SmallerVideo = styled.div`
+  background-color: #44e0f6;
+  border: 10px solid #44e0f6;
+  box-shadow: 10px 10px 0px 0px rgba(125, 250, 154, 1),
+    10px 10px 23px 2px rgba(0, 0, 0, 0.46);
 `;
 
-const Greeting = style.h1`
+const Greeting = styled.h1`
   font-family: "Gibson Bold";
   width: 100%;
   padding-bottom: 40px;
   text-align: center;
-  -webkit-text-fill-color: #7DFA9B;
-  -webkit-text-stroke-color: #44E0F6;
-  -webkit-text-stroke-width: 1.00px;
-  color: #7DFA9B;
+  -webkit-text-fill-color: #7dfa9b;
+  -webkit-text-stroke-color: #44e0f6;
+  -webkit-text-stroke-width: 1px;
+  color: #7dfa9b;
   font-size: 40px;
 `;
 
-const CreateGreeting = style.p`
-text-align: center;
-width: 100%;
-color: aquamarine;
-font-size: 20px;
-
+const CreateGreeting = styled.p`
+  text-align: center;
+  width: 100%;
+  color: aquamarine;
+  font-size: 20px;
 `;
 
 const StyledLink = styled(Link)`
